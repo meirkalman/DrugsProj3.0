913703801d20970656e2308a3fd70d29c5655b88
mk600449@gmail.com <mk600449@gmail.com>
2020-10-14 12:06:05 +0300

    BE start

---
+++ BE/Medicine.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE
+{
+    public class Medicine
+    {
+        public int Id { get; set; }
+        public string CommercialName { get; set; }
+        public string GenericName { get; set; }
+        public string Producer { get; set; }
+        public DateTime PeriodOfUse { get; set; }
+        public List<string> ActiveIngredients { get; set; }
+        public string ImageUri { get; set; }
+
+
+        public Medicine()
+        {
+            Id = 1;
+            CommercialName = "acamol";
+            GenericName = "acamol";
+            Producer = "teva";
+            PeriodOfUse = DateTime.Now;
+
+        }
+    }
+}
