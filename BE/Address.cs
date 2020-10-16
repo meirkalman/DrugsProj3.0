46d9c9f94fc85d896282ca9c33f7186a0b7fb666
mk600449@gmail.com <mk600449@gmail.com>
2020-10-16 15:14:58 +0300

    mvvm

---
+++ BE/Address.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE
+{
+    public class Address
+    {
+        public string City { get; set; }
+        public string Street { get; set; }
+        public int BuildingNumber { get; set; }
+
+        public Address()
+        {
+
+        }
+        public Address(string City, string Street, int BuildingNumber)
+        {
+            this.City = City;
+            this.Street = Street;
+            this.BuildingNumber = BuildingNumber;
+        }
+
+        public Address(Address address)
+        {
+            City = address.City;
+            Street = address.Street;
+            BuildingNumber = address.BuildingNumber;
+        }
+      
+        public override string ToString()
+        {
+            return Street + " " + BuildingNumber + " " + City;
+        }
+    }
+}
+
--- BE/Patient.cs
+++ BE/Patient.cs
@@ -11,22 +11,26 @@ namespace BE
         public int Id { get; set; }
         public string Fname { get; set; }
         public string Lname { get; set; }
-       // public int PhoneNumber { get; set; }
-    //    public List<Medicine> Medicines { get; set; }
-       // public string Address { get; set; }
-      //  public List<DoctorVisit> MedicalRecord { get; set; }
+        public int PhoneNumber { get; set; }
+        //    public List<Medicine> Medicines { get; set; }
+        //public Address PatientAddress { get; set; }
+        //  public List<DoctorVisit> MedicalRecord { get; set; }
 
         public DateTime DateOfBirth { get; set; }
 
-        public Patient(int id, string fname, string lname,/* int phoneNumber,*/ /*string address,*/ DateTime dateOfBirth)
+        public Patient(int id, string fname, string lname, int phoneNumber, /*Address PatientAddress,*/ DateTime dateOfBirth)
         {
             Id = id;
             Fname = fname;
             Lname = lname;
-            //  PhoneNumber = phoneNumber;
-          //  Address = address;
+            PhoneNumber = phoneNumber;
+            //PatientAddress =new Address(PatientAddress);
             DateOfBirth = dateOfBirth;
 
+        }
+
+        public Patient()
+        {
         }
     }
 }
