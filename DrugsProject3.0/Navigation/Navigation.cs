bdfd0ac241029bc47c40e2de68c097e63a227c65
yitzchakRavitz <yitzchak1992@gmail.con>
2020-10-19 20:42:31 +0200

    navigation

--- DrugsProject3.0/Navigation/Navigation.cs
+++ DrugsProject3.0/Navigation/Navigation.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DrugsProject3._0.Controls;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,9 +12,45 @@ namespace DrugsProject3._0.Navigation
     {
         public MainWindow wnd = new MainWindow();
         UserControl uc = new UserControl();
-        public void ShowControl()
+
+        public void ShowControl(UserControl uc)
         {
-            wnd.ShowControl(userControl);
+            wnd.MainWindowGrid.Children.Clear();
+            wnd.MainWindowGrid.Children.Add(uc);
+        }
+
+          
+        public void ShowControl(string UserControl)
+        {
+            switch (UserControl)
+            {
+                case "LoginUC":
+                    uc = new LoginUC();
+                    ShowControl(uc);
+                    break;
+                case "HomePage":
+                    uc = new HomePage();
+                    ShowControl(uc);
+                    break;
+                case "DoctorVisitUC":
+                    uc = new DoctorVisitUC();
+                    ShowControl(uc);
+                    break;
+                case "doctorUC":
+                    uc = new doctorUC();
+                    ShowControl(uc);
+                    break;
+                case "AdministratorUC":
+                    uc = new AdministratorUC();
+                    ShowControl(uc);
+                    break;
+                case "AddNewPatient":
+                    uc = new AddNewPatient();
+                    ShowControl(uc);
+                    break;
+               
+            }
+           
         }
     }
 }
