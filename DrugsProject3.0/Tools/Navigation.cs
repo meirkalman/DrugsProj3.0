15c5c2c2e0ed72a978d4ce13278321c68be0cee7
yitzchakRavitz <yitzchak1992@gmail.con>
2020-10-26 15:39:59 +0200

    ש

--- DrugsProject3.0/Tools/Navigation.cs
+++ DrugsProject3.0/Tools/Navigation.cs
@@ -50,6 +50,10 @@ namespace DrugsProject3._0.Navigation
                     uc = new AddMedicineUC();
                     title = "הוספת משתמש חדש";
                     break;
+                case "StatisticsUC":
+                    uc = new StatisticsUC();
+                    title = "היסטורית צריכת תרופות";
+                    break;
 
             }
             
