5260b57b88d4e255c84f99bd9cd199cfb2019c91
yitzchakRavitz <yitzchak1992@gmail.con>
2020-10-27 11:32:51 +0200

    df

---
+++ DrugsProject3.0/ViewModels/AdministratorInformationVM.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrugsProject3._0.ViewModels
+{
+    class AdministratorInformationVM
+    {
+        public AdministratorInformationModel AdministratorInformationM;
+
+
+        public ObservableCollection<Recipe> Recipes { get; set; }
+
+
+        public AdministratorInformationVM()
+        {
+            AdministratorInformationM = new AdministratorInformationModel\();
+
+            Command = new StatisticsCommand(this);
+            Recipes = new ObservableCollection<Recipe>(AdministratorInformationM.GetAllRecipes());
+        }
+
+        public StatisticsCommand Command { get; set; }
+        //  public DoctorCommand CommandAP { get; set; }
+
+    }
+}
