73baeaaffbe1e4ab9e1d4391b368923b2e9a506a
yitzchakRavitz <yitzchak1992@gmail.con>
2020-11-10 10:39:49 +0200

    PL

--- DrugsProject3.0/MainWindow.xaml.cs
+++ DrugsProject3.0/MainWindow.xaml.cs
@@ -42,62 +42,62 @@ namespace DrugsProject3._0
 
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            //Bl.AddRecipe(new BE.Recipe("1234", "999999999", "222222222", "8124", 30, 2, "כאב בטן", DateTime.Now.AddDays(-3)));
-            //Bl.AddRecipe(new BE.Recipe("1235", "888888888", "222222222", "199487", 40, 3, "כאב", DateTime.Now.AddDays(-10)));
-            //Bl.AddRecipe(new BE.Recipe("1236", "777777777", "222222222", "37418", 40, 3, "שלשול", DateTime.Now.AddDays(-20)));
-            //Bl.AddUser(new BE.User("111111111", "מאיר", "קלמן", "11111111", BE.User.UserType.ADMIN, "11111111"));
-            //Bl.AddUser(new BE.User("222222222", "יצחקי", "רביץ", "22222222", BE.User.UserType.DOCTOR, "22222222"));
-            //Bl.AddUser(new BE.User("333333333", "זאבי", "קמינסקי", "77", BE.User.UserType.DOCTOR, "33333333"));
-            //Bl.AddPatient(new BE.Patient("999999999", "משה", "כהן", "0527631366", DateTime.Now.AddDays(-100)));
-            //Bl.AddPatient(new BE.Patient("888888888", "יוסף", "ברקוביץ", "0527631366", DateTime.Now.AddDays(-200)));
-            //Bl.AddPatient(new BE.Patient("777777777", "הלל", "לוי", "0527631366", DateTime.Now.AddDays(-107)));
-            //Bl.AddMedicine(new BE.Medicine("8124", "phenelzine", "phenelzine", "טבע", "30", "gfjd"));
-            //Bl.AddMedicine(new BE.Medicine("199487", "moclobemide", "moclobemide", "טבע", "900", "ddd"));
-            //Bl.AddMedicine(new BE.Medicine("37418", "sumatriptan", "sumatriptan", "טבע", "87", "llll"));
-            //Bl.print(new BE.Recipe("1234", "999999999", "222222222", "8124", 30, 2, "כאב בטן", DateTime.Now.AddDays(-3)));
-            //Bl.createPDF(new BE.Recipe("1234", "999999999", "222222222", "8124", 30, 2, "כאב בטן", DateTime.Now.AddDays(-3)));
-            //string drugName = "rizatriptan";
-            //List<string> result = Bl.interactionDrugs(drugName);
-            //if (result != null)
-            //{
-            //    MessageBox.Show(result[0].ToString());
-            //}
-
-        }
-
-        private void Button1(object sender, RoutedEventArgs e)
-        {
-            Bl.AddRecipe(new BE.Recipe("1234", "phenelzine", "999999999", "222222222", "8124", 30, 2, "כאב בטן", DateTime.Now.AddDays(-500)));
-            Bl.AddRecipe(new BE.Recipe("1235", "moclobemide", "888888888", "222222222", "199487", 40, 3, "כאב", DateTime.Now.AddDays(-2000)));
-            Bl.AddRecipe(new BE.Recipe("1236", "sumatriptan", "777777777", "222222222", "37418", 40, 3, "שלשול", DateTime.Now.AddDays(-1000)));
-         //   Bl.AddUser(new BE.User("1", "מאיר", "קלמן", "11111111", BE.User.UserType.מנהל, "1"));
-         //   Bl.AddUser(new BE.User("2", "יצחקי", "רביץ", "22222222", BE.User.UserType.רופא, "2"));
-         //   Bl.AddUser(new BE.User("333333333", "זאבי", "קמינסקי", "77", BE.User.UserType.רופא, "33333333"));
-            //Bl.AddPatient(new BE.Patient("999999999", "משה", "כהן", "0527631366", DateTime.Now.AddDays(-100)));
-           // Bl.AddPatient(new BE.Patient("888888888", "יוסף", "ברקוביץ", "0527631366", DateTime.Now.AddDays(-200)));
-           // Bl.AddPatient(new BE.Patient("777777777", "הלל", "לוי", "0527631366", DateTime.Now.AddDays(-107)));
-            Bl.AddMedicine(new BE.Medicine("8123", "phenelzine", "phenelzine", "טבע", "30", "gfjd"));
-            Bl.AddMedicine(new BE.Medicine("199487", "moclobemide", "moclobemide", "טבע", "900", "ddd"));
-            Bl.AddMedicine(new BE.Medicine("37418", "sumatriptan", "sumatriptan", "טבע", "87", "llll"));
-            //Bl.AddRecipe(new BE.Recipe("456985", "999999999", "222222222", "8124", 30, 2, "כאב בטן", new DateTime(7/8/2010)));
-            //Bl.AddRecipe(new BE.Recipe("23225566", "888888888", "222222222", "199487", 40, 3, "כאב", new DateTime(7/8/2009)));
-            //Bl.AddRecipe(new BE.Recipe("5623652", "777777777", "222222222", "37418", 40, 3, "שלשול", new DateTime(7/8/2001)));
-        }
-        //    //phenelzine
-        //    moclobemide
-        //    isocarboxazid
-        //methylene blue
-        //tranylcypromine
         //private void Button_Click(object sender, RoutedEventArgs e)
         //{
-        //    this.ShowControl(uc);
+        //    //Bl.AddRecipe(new BE.Recipe("1234", "999999999", "222222222", "8124", 30, 2, "כאב בטן", DateTime.Now.AddDays(-3)));
+        //    //Bl.AddRecipe(new BE.Recipe("1235", "888888888", "222222222", "199487", 40, 3, "כאב", DateTime.Now.AddDays(-10)));
+        //    //Bl.AddRecipe(new BE.Recipe("1236", "777777777", "222222222", "37418", 40, 3, "שלשול", DateTime.Now.AddDays(-20)));
+        //    //Bl.AddUser(new BE.User("111111111", "מאיר", "קלמן", "11111111", BE.User.UserType.ADMIN, "11111111"));
+        //    //Bl.AddUser(new BE.User("222222222", "יצחקי", "רביץ", "22222222", BE.User.UserType.DOCTOR, "22222222"));
+        //    //Bl.AddUser(new BE.User("333333333", "זאבי", "קמינסקי", "77", BE.User.UserType.DOCTOR, "33333333"));
+        //    //Bl.AddPatient(new BE.Patient("999999999", "משה", "כהן", "0527631366", DateTime.Now.AddDays(-100)));
+        //    //Bl.AddPatient(new BE.Patient("888888888", "יוסף", "ברקוביץ", "0527631366", DateTime.Now.AddDays(-200)));
+        //    //Bl.AddPatient(new BE.Patient("777777777", "הלל", "לוי", "0527631366", DateTime.Now.AddDays(-107)));
+        //    //Bl.AddMedicine(new BE.Medicine("8124", "phenelzine", "phenelzine", "טבע", "30", "gfjd"));
+        //    //Bl.AddMedicine(new BE.Medicine("199487", "moclobemide", "moclobemide", "טבע", "900", "ddd"));
+        //    //Bl.AddMedicine(new BE.Medicine("37418", "sumatriptan", "sumatriptan", "טבע", "87", "llll"));
+        //    //Bl.print(new BE.Recipe("1234", "999999999", "222222222", "8124", 30, 2, "כאב בטן", DateTime.Now.AddDays(-3)));
+        //    //Bl.createPDF(new BE.Recipe("1234", "999999999", "222222222", "8124", 30, 2, "כאב בטן", DateTime.Now.AddDays(-3)));
+        //    //string drugName = "rizatriptan";
+        //    //List<string> result = Bl.interactionDrugs(drugName);
+        //    //if (result != null)
+        //    //{
+        //    //    MessageBox.Show(result[0].ToString());
+        //    //}
+
         //}
 
-        //private void Button_Click(object sender, RoutedEventArgs e)
+        //private void Button1(object sender, RoutedEventArgs e)
         //{
-        //    this.ShowControl(uc);Command="{Binding MainCommand}"
+        //    Bl.AddRecipe(new BE.Recipe("1234", "phenelzine", "999999999", "222222222", "8124", 30, 2, "כאב בטן", DateTime.Now.AddDays(-500)));
+        //    Bl.AddRecipe(new BE.Recipe("1235", "moclobemide", "888888888", "222222222", "199487", 40, 3, "כאב", DateTime.Now.AddDays(-2000)));
+        //    Bl.AddRecipe(new BE.Recipe("1236", "sumatriptan", "777777777", "222222222", "37418", 40, 3, "שלשול", DateTime.Now.AddDays(-1000)));
+        // //   Bl.AddUser(new BE.User("1", "מאיר", "קלמן", "11111111", BE.User.UserType.מנהל, "1"));
+        // //   Bl.AddUser(new BE.User("2", "יצחקי", "רביץ", "22222222", BE.User.UserType.רופא, "2"));
+        // //   Bl.AddUser(new BE.User("333333333", "זאבי", "קמינסקי", "77", BE.User.UserType.רופא, "33333333"));
+        //    //Bl.AddPatient(new BE.Patient("999999999", "משה", "כהן", "0527631366", DateTime.Now.AddDays(-100)));
+        //   // Bl.AddPatient(new BE.Patient("888888888", "יוסף", "ברקוביץ", "0527631366", DateTime.Now.AddDays(-200)));
+        //   // Bl.AddPatient(new BE.Patient("777777777", "הלל", "לוי", "0527631366", DateTime.Now.AddDays(-107)));
+        //    Bl.AddMedicine(new BE.Medicine("8123", "phenelzine", "phenelzine", "טבע", "30", "gfjd"));
+        //    Bl.AddMedicine(new BE.Medicine("199487", "moclobemide", "moclobemide", "טבע", "900", "ddd"));
+        //    Bl.AddMedicine(new BE.Medicine("37418", "sumatriptan", "sumatriptan", "טבע", "87", "llll"));
+        //    //Bl.AddRecipe(new BE.Recipe("456985", "999999999", "222222222", "8124", 30, 2, "כאב בטן", new DateTime(7/8/2010)));
+        //    //Bl.AddRecipe(new BE.Recipe("23225566", "888888888", "222222222", "199487", 40, 3, "כאב", new DateTime(7/8/2009)));
+        //    //Bl.AddRecipe(new BE.Recipe("5623652", "777777777", "222222222", "37418", 40, 3, "שלשול", new DateTime(7/8/2001)));
         //}
+        ////    //phenelzine
+        ////    moclobemide
+        ////    isocarboxazid
+        ////methylene blue
+        ////tranylcypromine
+        ////private void Button_Click(object sender, RoutedEventArgs e)
+        ////{
+        ////    this.ShowControl(uc);
+        ////}
+
+        ////private void Button_Click(object sender, RoutedEventArgs e)
+        ////{
+        ////    this.ShowControl(uc);Command="{Binding MainCommand}"
+        ////}
     }
 }
